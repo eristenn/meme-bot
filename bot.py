ff94daeb1a52a5dbe06c30ea9932d7ec4e49d350
Eristen <estroke.the.gamer@gmail.com>
2025-06-24 21:55:21 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord # type: ignore
+import requests # type: ignore
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('YOUR_DISCORD_TOKEN_HERE') # Replace with your actual token
